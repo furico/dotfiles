4f5338f8e3efafdb92d2264d4a0d5fe799eceb87
furico <furihatah@gmail.com>
2019-08-24 16:20:00 +0900

    fix setup.sh

--- setup.sh
+++ setup.sh
@@ -6,6 +6,6 @@ base_dir=$(pwd)
 
 # vim
 ln -sf ${base_dir}/vim/vimrc ${HOME}/.vimrc
-ln -sf ${base_dir}/vim/bundle/ ${HOME}/.vim/
+ln -sf ${base_dir}/vim/bundle/ ${HOME}/.vim
 # tmux
 ln -sf ${base_dir}/tmux/tmux.conf ${HOME}/.tmux.conf
