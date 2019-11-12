8efdae5d6ebd3b67ddc30b4574384b97023264c1
furi <furihatah@gmail.com>
2019-11-12 18:47:23 +0900

    Add emacs.d/init.el

--- setup.sh
+++ setup.sh
@@ -7,5 +7,7 @@ base_dir=$(pwd)
 # vim
 ln -sf ${base_dir}/vim/vimrc ${HOME}/.vimrc
 ln -sf ${base_dir}/vim/bundle/ ${HOME}/.vim
+# emacs
+ln -sf ${base_dir}/emacs.d/ ${HOME}/.emacs.d
 # tmux
 ln -sf ${base_dir}/tmux/tmux.conf ${HOME}/.tmux.conf
