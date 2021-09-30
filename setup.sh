941a1af264aec24d9d2a824b4e654817b8c5fea2
furico <furihatah@gmail.com>
2021-09-30 22:51:14 +0900

    Add config/nvim/init.lua

--- setup.sh
+++ setup.sh
@@ -6,6 +6,7 @@ base_dir=$(pwd)
 
 # vim
 ln -sf ${base_dir}/vim/vimrc ${HOME}/.vimrc
-" ln -sf ${base_dir}/vim/pack/ ${HOME}/.vim
+ln -sf ${base_dir}/vim/pack/ ${HOME}/.vim
 # tmux
 ln -sf ${base_dir}/tmux/tmux.conf ${HOME}/.tmux.conf
+ln -sf ${base_dir}/config/nvim/ ${HOME}/.config/
