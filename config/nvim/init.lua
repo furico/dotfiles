5aaf40a379f8f462fe87483225bb520bde9fb870
furico <furihatah@gmail.com>
2023-05-17 22:27:34 +0900

    fix

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -33,6 +33,7 @@ require("lazy").setup({
 	'hoob3rt/lualine.nvim',
 	'rktjmp/lush.nvim',
 	'ellisonleao/gruvbox.nvim',
+	'sindrets/diffview.nvim',
 	{
 		'nvim-telescope/telescope.nvim',
 		tag = '0.1.1',
@@ -102,6 +103,7 @@ local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+vim.keymap.set('n', '<leader>fc', builtin.commands, {})
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
 
 -- nvim-tree
