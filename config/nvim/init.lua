c7f11891bb8498b13d398337753774d72c6a9765
furi <furihatah@gmail.com>
2023-05-30 17:32:24 +0900

    fix

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -157,6 +157,22 @@ require("lazy").setup {
     end,
   },
   "hashivim/vim-terraform",
+  {
+    "nvim-pack/nvim-spectre",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+    keys = {
+      { "<leader>S", '<cmd>lua require("spectre").open()<CR>', desc = "Open Spectre" },
+      { "<leader>sw", '<cmd>lua require("spectre").open_visual({select_word=true})<CR>', desc = "Search current word" },
+      { "<leader>sw", '<esc><cmd>lua require("spectre").open_visual()<CR>', mode = "v", desc = "Search current word" },
+      {
+        "<leader>sp",
+        '<cmd>lua require("spectre").open_file_search({select_word=true})<CR>',
+        desc = "Search on current file",
+      },
+    },
+  },
 }
 
 ----------------------------------------
